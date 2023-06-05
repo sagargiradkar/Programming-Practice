d3e372477ee4b6b142d00c89b1e4b610db68dbf5
sagar giradkar <spgiradkar2002@gmail.com>
2023-06-06 02:09:14 +0530

    tans

--- Graphics/ass6.cpp
+++ Graphics/ass6.cpp
@@ -8,7 +8,7 @@ ii) Other transformations iii) Composite Transformations
 using namespace std;
 #define h 700
 #define w 700                                                                                                                                                               
-int mati[100][100], k = 0, c;
+int mati[100][100], k = 0;
 int mato[100][100]; 
 float tx,ty;
 float Xm,Ym;
